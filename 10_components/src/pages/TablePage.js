936b77051bf266cc079d9b35a3e8d4ce40b62959
kyriosaa <kyriosaaph@gmail.com>
2024-10-20 04:56:28 +0800

    16: Data Sorting pt2

--- 10_components/src/pages/TablePage.js
+++ 10_components/src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -12,6 +12,7 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
@@ -21,6 +22,7 @@ function TablePage() {
       label: "Score",
       render: (fruit) => fruit.score,
       header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -30,7 +32,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
