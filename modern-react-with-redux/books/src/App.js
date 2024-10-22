f3e45f885f56edba8fd5076bfb52a94be600f0a3
kyriosaa <kyriosaaph@gmail.com>
2024-10-22 12:47:08 +0800

    Update names

---
+++ modern-react-with-redux/books/src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BooksContext from "./context/books";
+
+function App() {
+  const { handleFetchBooks } = useContext(BooksContext);
+
+  useEffect(() => {
+    handleFetchBooks();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
+    </div>
+  );
+}
+
+export default App;
---
+++ modern-react-with-redux/rtk/README.md
@@ -0,0 +1,3 @@
+Created on 19/09/2024
+
+Modern React and Redux class - Udemy
